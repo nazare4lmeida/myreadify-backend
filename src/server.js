d41979c56da14e4b4a036eda0d8d7ff7c3ba3440
Nazare Almeida <nazyalmeida@gmail.com>
2025-07-22 14:37:48 -0300

    fix: Corrige caminhos e variável de porta para deploy na Vercel

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 // src/server.js
 const app = require('./app');
 
-const port = process.env.APP_PORT || 3333;
+const port = process.env.PORT || 3333;
 
 app.listen(port, () => {
   console.log(`🚀 Servidor rodando na porta ${port}`);
