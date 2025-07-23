f8bed6930d2b3574c90519a60dcbe7ad05bf0a96
Nazare Almeida <nazyalmeida@gmail.com>
2025-07-23 14:59:29 -0300

    adicionado arquivo .env.example

--- src/server.js
+++ src/server.js
@@ -1,9 +1,23 @@
-const app = require('./app');
-require('../swagger')(app);
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development',
+});
 
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+const swaggerDocs = require('../swagger');
 
-const port = process.env.PORT || 3333;
+require('./config/database'); 
 
-app.listen(port, () => {
-  console.log(`🚀 Servidor rodando na porta ${port}`);
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+swaggerDocs(app);
+
+const PORT = process.env.APP_PORT || 3333;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server rodando na porta ${PORT}`);
 });
