f6927305b231a25124fa0d8339cdea5f34c8458d
Nazare Almeida <nazyalmeida@gmail.com>
2025-07-23 15:40:12 -0300

    fix: correção nas rotas

--- src/server.js
+++ src/server.js
@@ -13,7 +13,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(routes);
+
+app.use('/api', routes);
+
 swaggerDocs(app);
 
 const PORT = process.env.APP_PORT || 3333;
